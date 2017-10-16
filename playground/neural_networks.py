3a80a337c5384a991a9efe80b0ea53c33435dd81
Anthony Lee <antwonlee@gmail.com>
2017-10-15 19:32:04 -0500

    Add ANN

--- playground/neural_networks.py
+++ playground/neural_networks.py
@@ -1,1 +1,12 @@
 import numpy as np
+
+class NeuralNetwork(object):
+    def __init__(self):
+        # Define HyperParameters
+        self.input_layer_size = 2
+        self.output_layer_sie = 1
+        self.hidden_layer_size = 3
+
+    def forward(self, X):
+        # Propagate inputs through network
+        return
