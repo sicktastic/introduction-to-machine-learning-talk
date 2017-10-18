a14dc3426c8286fe990df9c19ff9e5bc504fa1f3
Anthony Lee <antwonlee@gmail.com>
2017-10-18 13:30:09 -0500

    Add comments

--- mnist_tensorflow.py
+++ mnist_tensorflow.py
@@ -1,16 +1,17 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
+# import mnist data
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 x = tf.placeholder(tf.float32, [None, 784])
 
-# Weight
+# weight
 W = tf.Variable(tf.zeros([784, 10]))
 
-# Bias
+# bias
 b = tf.Variable(tf.zeros([10]))
 
 y = tf.nn.softmax(tf.matmul(x, W) + b)
